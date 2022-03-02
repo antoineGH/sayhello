830811cfb4c7eaefbd9443e753e8fea24ab8bb86
Antoine Ratat <antoine.ratat@gmail.com>
2022-03-02 17:38:35 +0800

    add title and edit button to my goal

--- src/components/goal/Goal.tsx
+++ src/components/goal/Goal.tsx
@@ -1,23 +1,30 @@
-import { Card, Divider, Statistic, Timeline } from 'antd'
+import { Button, Card, Divider, Statistic, Timeline, Typography } from 'antd'
 import './style.css'
 
 const Goal = () => {
+	const { Title } = Typography
 	const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 	return (
-		<Card bordered={false} style={{ width: 350 }}>
-			<Statistic title='THIS WEEK' value='0 of 5 days' />
-			<Divider dashed />
-			<Timeline>
-				{weekDays.map((days, count) => {
-					count++
-					return (
-						<Timeline.Item key={count} color={count === 3 || count === 2 ? 'green' : 'red'}>
-							{days}
-						</Timeline.Item>
-					)
-				})}
-			</Timeline>
-		</Card>
+		<>
+			<div className='goal_title'>
+				<Title level={3}>My Goals</Title>
+				<Button type='link'>Edit</Button>
+			</div>
+			<Card bordered={false} style={{ width: 350 }}>
+				<Statistic title='THIS WEEK' value='0 of 5 days' />
+				<Divider dashed />
+				<Timeline>
+					{weekDays.map((days, count) => {
+						count++
+						return (
+							<Timeline.Item key={count} color={count === 3 || count === 2 ? 'green' : 'red'}>
+								{days}
+							</Timeline.Item>
+						)
+					})}
+				</Timeline>
+			</Card>
+		</>
 	)
 }
 export default Goal
