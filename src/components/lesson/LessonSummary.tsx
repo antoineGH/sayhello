c1847d350041ebf22694cd206305204ba5037555
Antoine Ratat <antoine.ratat@gmail.com>
2022-04-15 22:38:39 +0800

    design lesson summary

--- src/components/lesson/LessonSummary.tsx
+++ src/components/lesson/LessonSummary.tsx
@@ -27,29 +27,28 @@ const LessonSummary = ({
 	return (
 		<>
 			<Row>
+				<Col span={22}>
+					<div className='ant-statistic-title'>LESSON</div>
+				</Col>
+				<Col span={1} offset={1} style={{ justifyContent: 'center', display: 'flex' }}>
+					<Tooltip title={completed ? `${lessonName} completed` : `${lessonName} not completed`}>
+						{completed && <CheckSquareOutlined className='checkedLesson' />}
+					</Tooltip>
+				</Col>
+			</Row>
+			<Row>
 				<Col span={4}>
 					<Title level={4}>{lessonName}</Title>
 				</Col>
 			</Row>
 			<Row>
-				<Col span={1} offset={23} style={{ justifyContent: 'center', display: 'flex' }}>
-					<Tooltip title={completed ? `${lessonName} completed` : `${lessonName} not completed`}>
-						{completed ? (
-							<CheckSquareOutlined className='checkedLesson' />
-						) : (
-							<BorderOutlined className='uncheckedLesson' />
-						)}
-					</Tooltip>
-				</Col>
-			</Row>
-			<Row style={{ marginTop: '1rem' }}>
 				<Col span={4}>
-					<Statistic title='Duration' value={duration} suffix='h' />
+					<Statistic className='stat_course_lower' title='Duration' value={duration} suffix='h' />
 				</Col>
 				<Col span={4}>
-					<Statistic title='Difficulty' value={difficulty} suffix='/10' />
+					<Statistic className='stat_course_lower' title='Difficulty' value={difficulty} suffix='/10' />
 				</Col>
-				<Col span={1} offset={15} style={{ justifyContent: 'center', display: 'flex' }}>
+				<Col span={1} offset={15} className='col_course_summary_btn'>
 					<Button style={{ marginTop: '1rem' }} onClick={() => handleLesson(lessonID)}>
 						{completed ? 'Resume' : 'Start'}
 					</Button>
