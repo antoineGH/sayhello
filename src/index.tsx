56bf8098537ba9c9dee11bbe9d0ecc966df4cedb
Antoine Ratat <antoine.ratat@gmail.com>
2022-05-20 01:13:42 +0800

    create store and hook rootstate and appdispatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from 'store'
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 )
