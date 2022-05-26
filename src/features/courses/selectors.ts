999441ba2183415ed8b35f13b7774ba4931320a6
Antoine Ratat <antoine.ratat@gmail.com>
2022-05-27 03:30:40 +0800

    create slice, entity adapter and selectors

--- src/features/courses/selectors.ts
+++ src/features/courses/selectors.ts
@@ -1,1 +1,16 @@
-export {}
+import { createSelector } from '@reduxjs/toolkit'
+import { RootState } from 'store'
+import { coursesAdapter } from './slice'
+
+export const coursesSelectors = coursesAdapter.getSelectors(
+  (state: RootState) => state.courses
+)
+
+const getSlice = (state: RootState) => state.courses
+
+export const coursesIsLoading = createSelector(getSlice, state => state.loading)
+export const coursesHasError = createSelector(getSlice, state => state.error)
+export const coursesActiveID = createSelector(
+  getSlice,
+  state => state.courseActiveID
+)
--- src/features/courses/slice.ts
+++ src/features/courses/slice.ts
@@ -1,1 +1,18 @@
-export {}
+import { Course } from 'types/course'
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
+
+export const coursesAdapter = createEntityAdapter({
+  selectId: (course: Course) => course.id
+})
+export const coursesSlice = createSlice({
+  name: 'courses',
+  initialState: coursesAdapter.getInitialState({
+    loading: false,
+    error: false,
+    courseActiveID: 0
+  }),
+  reducers: {},
+  extraReducers: {}
+})
+
+export default coursesSlice.reducer
