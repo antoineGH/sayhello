0bdb34fb36d30750f81e6ed7f64d29c722d65fd0
Antoine Ratat <antoine.ratat@gmail.com>
2022-06-06 13:31:14 +0800

    selector results

--- src/features/results/selectors.ts
+++ src/features/results/selectors.ts
@@ -1,1 +1,12 @@
-export {}
+import { createSelector } from '@reduxjs/toolkit'
+import { RootState } from 'store'
+import { resultsAdapter } from './slice'
+
+export const ResultsSelector = resultsAdapter.getSelectors(
+  (state: RootState) => state.results
+)
+
+const getSlice = (state: RootState) => state.results
+
+export const resultIsLoading = createSelector(getSlice, state => state.loading)
+export const resultHasError = createSelector(getSlice, state => state.error)
